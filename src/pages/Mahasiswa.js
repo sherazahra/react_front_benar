9440ded9f187aea7ec4683272e5414bc139003ed
sherazahra <sherazahra30@gmail.com>
2023-10-24 15:22:28 +0700

    part 22

--- src/pages/Mahasiswa.js
+++ src/pages/Mahasiswa.js
@@ -42,6 +42,7 @@ function Mahasiswa() {
 
   const handleIdJurusanChange = (e) => {
     setIdJurusan(e.target.value);
+    console.log(id_j);
   };
 
   const handleGambarChange = (e) => {
@@ -65,7 +66,7 @@ function Mahasiswa() {
     formData.append("swa_foto", swa_foto);
 
     try {
-      await axios.post("http://localhost:5000/api/mhs/store", formData, {
+      await axios.post(`http://localhost:5000/api/mhs/store`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
@@ -77,6 +78,66 @@ function Mahasiswa() {
       setValidation(error.response.data);
     }
   };
+
+  const [editData, setEditData] = useState({
+    id: null,
+    nama: '',
+    nrp: '',
+    id_j : ''
+  });
+
+  const [showEditModal, setShowEditModal] = useState(false);
+
+  const handleShowEditModal = (data) => {
+    setEditData(data);
+    setShowEditModal(true);
+    setShow(false);
+  };
+
+  const handleCloseEditModal = () => {
+    setShowEditModal(false);
+    setEditData(null);
+  };
+
+  const handleEditDataChange = (field,value) => {
+    setEditData((prevData) => ({
+      ...prevData,
+      [field]: value,
+    }));
+  };
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+  
+    formData.append('id', editData.id);
+    formData.append('nama', editData.nama);
+    formData.append('nrp', editData.nrp);
+    formData.append('jurusan', editData.id_j);
+  
+    if (editData.gambar) {
+      formData.append('gambar', editData.gambar);
+    }
+  
+    if (editData.swa_foto) {
+      formData.append('swa_foto', editData.swa_foto);
+    }
+  
+    try {
+      await axios.patch(`http://localhost:5000/api/mhs/update/${editData.id_m}`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      navigate('/mhs');
+      fetchData();
+      setShowEditModal(false);
+    } catch (error) {
+      console.error('Kesalahan:', error);
+      setValidation(error.response.data);
+    }
+  };
+  
     return (
         <Container>
             <Row>
@@ -102,6 +163,7 @@ function Mahasiswa() {
                                 <td>{mh.nrp}</td>
                                 <td><img src={url + mh.gambar} height="100" /></td>
                                 <td><img src={url + mh.swa_foto} height="100" /></td>
+                                <td> <button onClick={() => handleShowEditModal(mh)} className='btn btn-sm btn-info'> Edit </button></td>
                             </tr>
                         ))}
                     </tbody>
@@ -135,10 +197,9 @@ function Mahasiswa() {
               <label className="form-label">Jurusan:</label>
               <select
                 className="form-select"
-                defaultValue={"1"}
                 onChange={handleIdJurusanChange}
               >
-                <option selected value={""}>
+                <option selected value={""} >
                   Select Jurusan
                 </option>
                 {jrs.map((jr, idx) => (
@@ -176,6 +237,69 @@ function Mahasiswa() {
           </form>
         </Modal.Body>
       </Modal>
+      <Modal show={showEditModal} onHide={handleCloseEditModal}>
+              <Modal.Header closeButton>
+                <Modal.Title>Edit Data</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <form onSubmit={handleUpdate}>
+                  <div className="mb-3">
+                    <label className="form-label">Nama:</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      value={editData ? editData.nama : ''}
+                      onChange={(e) => handleEditDataChange('nama', e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">NRP:</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      value={editData ? editData.nrp : ''}
+                      onChange={(e) => handleEditDataChange('nrp', e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Jurusan:</label>
+                    <select
+                      className="form-select"
+                      value={editData ? editData.id_j : ''}
+                      onChange={(e) => handleEditDataChange('id_j', e.target.value)}
+                    >
+                      {jrs.map((jr) => (
+                        <option key={jr.id_j} value={jr.id_j} selected={editData.id_j === jr.id_j ? true : false}>
+                          {jr.nama_jurusan}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Gambar:</label>
+                    <input
+                      type="file"
+                      className="form-control"
+                      accept="image/*"
+                      onChange={(e) => handleEditDataChange('gambar', e.target.files[0])}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Swa Foto:</label>
+                    <input
+                      type="file"
+                      className="form-control"
+                      // value={editData ? editData.swa_foto : ''}
+                      accept="image/*"
+                      onChange={(e) => handleEditDataChange('swa_foto', e.target.files[0])}
+                    />
+                  </div>
+                  <button type="submit" className="btn btn-primary">
+                    Simpan Perubahan
+                  </button>
+                  </form>
+              </Modal.Body>
+            </Modal>
         </Container>
     )
 }
